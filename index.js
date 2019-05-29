321594e3f5dbfb8de35f617a2d026674b2ca8865
Daniel Ang <dan.justin.ang@gmail.com>
2019-05-29 13:02:05 -0400

    installed bcrypt, express-validator, and jsonwebtoken, also added the user model and ability to register a user

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
-
 const app = express()
-const port = 5000
+const port = process.env.PORT || 3000
+const connectDB = require('./config/db')
+
+//Connect to the DB
+connectDB()
+
+// Init Middleware
+// Already part of Express now, don't need to install body parser
+app.use(express.json({extended: false}))
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'))
+
 
 app.get('/', (req, res) => {
-  res.send("hello world")
+  res.json({msg: "hello"})
 })
 
 app.listen(port, () => {console.log(`Running on ${port}`)})
---
+++ routes/api/users.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const { check, validationResult } = require("express-validator/check");
+const User = require("../../models/User");
+
+// GET api/users
+//Public
+router.get("/", (req, res) => {
+  res.send("User Route");
+});
+
+//POST User
+router.post(
+  "/register",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password is required")
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "email already in use" }] });
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+          name,
+          email
+        }
+      }
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({token})
+        }
+      )
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+module.exports = router;
