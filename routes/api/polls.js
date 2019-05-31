fb06c6129070b15f006758e14924f955a8b9aaa7
Daniel Ang <dan.justin.ang@gmail.com>
2019-05-30 22:27:37 -0400

    fixed delte function to remove polls within the user, currently working on voting function

--- routes/api/polls.js
+++ routes/api/polls.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const auth = require('../../middleware/auth')
+const auth = require("../../middleware/auth");
 
 const Poll = require("../../models/Poll");
 const User = require("../../models/User");
@@ -17,124 +17,136 @@ router.get("/", (req, res) => {
 //@access Public
 
 router.get("/all", async (req, res) => {
-
   try {
     const polls = await Poll.find();
     res.status(200).json({ polls });
   } catch (err) {
-    console.log(err.message)
-    res.status(500).res('Server Error')
+    console.log(err.message);
+    res.status(500).res("Server Error");
   }
 });
-
 
 //@route POST api/poll/create
 //@desc - Create a poll
 //@access Private
 
-router.post('/', auth, async (req,res) => {
-
+router.post("/", auth, async (req, res) => {
   try {
     // console.log(req.user)
-    const { id } = req.user
-    const user = await User.findById(id).select("-password")
-    console.log(user)
+    const { id } = req.user;
+    const user = await User.findById(id).select("-password");
+    console.log(user);
 
-    const {question, options} = req.body
+    const { question, options } = req.body;
     const poll = await Poll.create({
       question,
       user,
-      options: options.map(option => ({ option,
-        votes: 0 }))
+      options: options.map(option => ({ option, votes: 0 }))
     });
 
     user.polls.push(poll.id);
     // await user.save()
-    console.log(user)
+    console.log(user);
 
-    await user.save()
+    await user.save();
 
     // console.log(user.polls)
-    res.status(201).json(poll)
+    res.status(201).json(poll);
   } catch (err) {
-    console.log(err.message)
-    res.status(400).json({msg: "Redirect"})
+    console.log(err.message);
+    res.status(400).json({ msg: "Redirect" });
   }
-})
+});
 
 //@route GET api/poll/user
 //@desc GET - Your own poll
 //@access Private
 
-router.get('/user', auth, async (req, res) => {
+router.get("/user", auth, async (req, res) => {
   try {
-    const { id, email } = req.user
+    const { id, email } = req.user;
 
-    const user = await User.findById(id).populate('polls')
+    const user = await User.findById(id).populate("polls");
 
-    res.status(200).json(user.polls)
-
+    res.status(200).json(user.polls);
   } catch (err) {
-    console.log(err.message)
-    res.status(500).res('Server Error')
+    console.log(err.message);
+    res.status(500).res("Server Error");
   }
-})
+});
 
 //@route GET api/poll/:id
 //@desc GET - Your own poll
 //@access Public
 
-router.get('/:id', async (req, res) => {
-  try{
-    const {id} = req.params;
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
 
-    const poll = await Poll.findById(id).populate(
-      'user',
-      ['name', 'id']
-    );
+    const poll = await Poll.findById(id).populate("user", ["name", "id"]);
 
-    if(!poll) return res.status(400).json({msg: "Poll not found"})
+    if (!poll) return res.status(400).json({ msg: "Poll not found" });
 
-    res.status(200).json(poll)
-  } catch(err) {
-    console.log(err)
-    res.status(500).res('Server Error')
+    res.status(200).json(poll);
+  } catch (err) {
+    console.log(err);
+    res.status(500).res("Server Error");
   }
-})
+});
 
-//@route Delete api/poll
+//@route Delete api/poll/:id
 //@desc DELETE your poll
 //@access Public
 
-router.delete('/:id', auth, async (req, res) => {
-  try{
-    const {id: pollId} = req.params;
-    const {id: userId} = req.user;
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const { id: pollId } = req.params;
+    const { id: userId } = req.user;
 
-    const poll = await Poll.findById(pollId)
+    const poll = await Poll.findById(pollId);
 
-    if(!poll) return res.status(400).json({msg: "Poll not found"})
+    if (!poll) return res.status(400).json({ msg: "Poll not found" });
     if (poll.user.toString() != userId) {
-      return res.status(400).json({msg: "Unathorized Access"})
+      return res.status(400).json({ msg: "Unathorized Access" });
     }
 
+    const user = await User.findById(userId);
+    console.log(user.polls)
+    console.log(pollId.toString())
+
+    user.polls.splice(pollId.toString(), 1)
+
+    await user.save()
     await poll.remove()
+
     res.status(202).json({msg: "Poll removed"})
-
-  } catch(err) {
-    console.log(err)
-    res.status(500).res('Server Error')
-  }
-})
-
-//@route GET api/poll/:poll_id
-//@desc - See the poll
-
-router.get('/:poll_id', auth, async (req, res) => {
-  try {
-
   } catch (err) {
-
+    console.log(err);
+    res.status(500).res("Server Error");
   }
-})
+});
+
+//@route POST api/poll/:id
+//@desc Vote for a poll
+//@access Public
+
+router.post("/:id", auth, async (req, res) => {
+  try {
+    const { id: pollId } = req.params;
+    const { id: userId } = req.user;
+    const { answer } = req.body;
+    console.log(answer);
+    if (answer) {
+      const poll = Poll.findById(id);
+      if (!poll) return res.status(400).json({ msg: "Poll not found" });
+
+      // const vote = poll.options.map(option => {
+      //   option.option === answer ?
+      // })
+    }
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).res("Server Error");
+  }
+});
 module.exports = router;
