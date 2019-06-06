97e61d9679ccad04243295dde98403eedfa20874
Daniel Ang <dan.justin.ang@gmail.com>
2019-06-06 18:38:35 -0400

    added store

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,24 +1,29 @@
-import React, {Fragment} from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Navbar from './layouts/Navbar'
-import Landing from './layouts/Landing'
-import Register from './auth/Register'
-import Login from './auth/Login'
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./layouts/Navbar";
+import Landing from "./layouts/Landing";
+import Register from "./auth/Register";
+import Login from "./auth/Login";
+
+//Redux
+import { Provider } from "react-redux";
+import store from "../store";
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar/>
-      <Route exact path ="/" component = {Landing}/>
-      <section className='container'>
-        <Switch>
-          <Route exact path ='/register' component = {Register}/>
-          <Route exact path ='/login' component = {Login}/>
-        </Switch>
-      </section>
-    </Fragment>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
+);
 
-  </Router>
-)
-
-export default App
+export default App;
