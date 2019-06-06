54de1eb0c8a4c1be3763b1a085857da1ad7e935b
Daniel Ang <dan.justin.ang@gmail.com>
2019-06-06 18:33:33 -0400

    added login and registration react components

--- server/client/src/components/auth/Login.js
+++ server/client/src/components/auth/Login.js
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, { Fragment, useState} from 'react';
+import { Link } from 'react-router-dom'
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = formData
+
+  const onChange = (e) => {
+    setFormData({...formData, [e.target.name]:e.target.value})
+  }
+  const onSubmit = async e => {
+    e.preventDefault()
+    if(password !== password2) {
+      console.log("password do not match")
+    } else {
+      console.log("SUCCESS")
+
+      // const newUser = {
+      //   name,
+      //   email,
+      //   password,
+      // }
+      // try {
+      //   const config ={
+      //     headers: {
+      //       'Content-Type': 'application/json'
+      //     }
+      //   }
+      //   const body = JSON.stringify(newUser);
+      //   console.log("body")
+      //   const res = await axios.post('/api/users/register', body, config);
+      //   console.log(res.data)
+      // } catch (err) {
+      //   console.error(err.response.data)
+      // }
+    }
+  }
   return(
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead"><i className="fas fa-user"></i> Sign into your account</p>
+
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" value={email} onChange={e => onChange(e)} name="email" />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password} onChange={e => onChange(e)}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an Account? <Link to="/register">Sign In</Link>
+      </p>
+    </Fragment>
   )
 }
 
--- server/client/src/components/layouts/Landing.js
+++ server/client/src/components/layouts/Landing.js
@@ -1,19 +1,16 @@
-import React from 'react';
-
+import React from "react";
+import { Link } from "react-router-dom";
 const Landing = () => {
-  return(
+  return (
     <section className="landing">
       <h1 className="x-large"> Polling Votes! </h1>
-      <p>
-        Vote for a poll
-      </p>
+      <p>Vote for a poll</p>
       <div className="buttons">
-        <a href="#">Sign Up</a>
-        <a href="#">Login</a>
+        <Link to="/register">Register</Link>
+        <Link to="/login"> Login</Link>
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
