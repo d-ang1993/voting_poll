df9ce46d893ec1792b603b02580dc14523d41a0c
Daniel Ang <dan.justin.ang@gmail.com>
2019-06-14 15:05:43 -0400

    added actions and reducers to register users

--- server/client/src/components/auth/Register.js
+++ server/client/src/components/auth/Register.js
@@ -1,28 +1,30 @@
-import React, { Fragment, useState} from 'react';
-import { connect } from 'react-redux'
+import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 // import axios from 'axios';
-import { Link } from 'react-router-dom'
-import { setAlert } from '../../actions/alert'
-import PropTypes from 'prop-types'
+import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import { register } from "../../actions/auth";
+import PropTypes from "prop-types";
 
-const Register = (props) => {
+const Register = props => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    password2: ''
+    name: "",
+    email: "",
+    password: "",
+    password2: ""
   });
 
-  const { name, email, password, password2 } = formData
+  const { name, email, password, password2 } = formData;
 
-  const onChange = (e) => {
-    setFormData({...formData, [e.target.name]:e.target.value})
-  }
+  const onChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
   const onSubmit = async e => {
-    e.preventDefault()
-    if(password !== password2) {
-      props.setAlert('Passwords do not match', 'danger')
+    e.preventDefault();
+    if (password !== password2) {
+      props.setAlert("Passwords do not match", "danger");
     } else {
+      props.register({ name, email, password });
 
       // const newUser = {
       //   name,
@@ -43,25 +45,41 @@ const Register = (props) => {
       //   console.error(err.response.data)
       // }
     }
-  }
-  return(
+  };
+  return (
     <Fragment>
       <h1 className="large text-primary">Sign Up</h1>
-      <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+      <p className="lead">
+        <i className="fas fa-user" /> Create Your Account
+      </p>
 
       <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
-          <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={e => onChange(e)}
+            // required
+          />
         </div>
         <div className="form-group">
-          <input type="email" placeholder="Email Address" value={email} onChange={e => onChange(e)} name="email" />
+          <input
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={e => onChange(e)}
+            name="email"
+          />
         </div>
         <div className="form-group">
           <input
             type="password"
             placeholder="Password"
             name="password"
-            value={password} onChange={e => onChange(e)}
+            value={password}
+            onChange={e => onChange(e)}
             minLength="6"
           />
         </div>
@@ -70,21 +88,26 @@ const Register = (props) => {
             type="password"
             placeholder="Confirm Password"
             name="password2"
-            value={password2} onChange={e => onChange(e)}
+            value={password2}
+            onChange={e => onChange(e)}
             minLength="6"
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <Link to='/login'>Login</Link>
+        Already have an account? <Link to="/login">Login</Link>
       </p>
     </Fragment>
-  )
-}
+  );
+};
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
-}
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired
+};
 
-export default connect(null, {setAlert})(Register)
+export default connect(
+  null,
+  { setAlert, register }
+)(Register);
--- server/client/src/reducers/index.js
+++ server/client/src/reducers/index.js
@@ -1,3 +1,8 @@
 import { combineReducers } from "redux";
-import alert from './alert'
-export default combineReducers({alert});
+import alert from "./alert";
+import auth from "./auth";
+
+export default combineReducers({
+      alert,
+      auth,
+});
